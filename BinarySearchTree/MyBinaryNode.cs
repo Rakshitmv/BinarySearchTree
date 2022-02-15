ae5de3a21b57737e3ee532b9a4f313c5da04d29e
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-15 19:39:17 +0530

    [Rakshit] Add: UC3_Ability to search 63 in binary tree

--- BinarySearchTree/MyBinaryNode.cs
+++ BinarySearchTree/MyBinaryNode.cs
@@ -51,7 +51,34 @@ namespace BinarySearchTree
         }
         public void GetSize()     //for size calculation
         {
-            Console.WriteLine("Size of the tree is"  + (1 + this.leftCount + this.rightCount));
+            Console.WriteLine("Size of the tree is" + (1 + this.leftCount + this.rightCount));
+        }
+
+        public bool IfExists(T element, MyBinaryNode<T> node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            if (node.nodeData.Equals(element))
+            {
+                Console.WriteLine("Got the value in BST " + node.nodeData);
+                result = true;
+            }
+            else
+            {
+                Console.WriteLine("Current value is {0} in BST ", node.nodeData);
+            }
+            if (element.CompareTo(node.nodeData) < 0)
+            {
+                IfExists(element, node.leftTree);
+            }
+            if (element.CompareTo(node.nodeData) > 0)
+            {
+                IfExists(element, node.rightTree);
+            }
+
+            return result;
         }
 
         public void Display()
@@ -69,5 +96,8 @@ namespace BinarySearchTree
             }
         }
 
+
+
     }
 }
+
