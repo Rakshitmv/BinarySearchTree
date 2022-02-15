b6cee542e2173b273509c7e781f58fa04c1e628b
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-15 19:16:07 +0530

    UC1_Create a BST by adding given values

---
+++ BinarySearchTree/MyBinaryNode.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class MyBinaryNode<T> where T : IComparable<T>
+    {
+        public T nodeData { get; set; }
+        public MyBinaryNode<T> leftTree { get; set; }
+
+        public MyBinaryNode<T> rightTree { get; set; }
+
+        public MyBinaryNode(T data)
+        {
+            this.nodeData = data;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T CurrNodeVal = this.nodeData;
+            if ((CurrNodeVal.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new MyBinaryNode<T>(item);
+                }
+                else
+                {
+                    this.leftTree.Insert(item);
+                }
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new MyBinaryNode<T>(item);
+                }
+                else
+                {
+                    this.rightTree.Insert(item);
+
+                }
+            }
+        }
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.nodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Binary Search Tree Operation");
+            MyBinaryNode<int> tree = new MyBinaryNode<int>(56);
+
+            Console.WriteLine("Enter 1: Add Values\n" + "Enter 2: Exit\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    tree.Insert(30);
+                    tree.Insert(70);
+                    tree.Display();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter correct option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
